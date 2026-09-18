b20e63b423f751f37a3b4dc65fb73d9f62268e93
NUSRAT KARIM <nusratsaleem123@gmail.com>
2026-09-18 19:53:32 +0500

    Fix import path for create_retrieval_chain

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
