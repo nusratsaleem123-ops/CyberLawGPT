eeb3b72d1db3f7763723db5dd2bbbe717b4aec98
NUSRAT KARIM <nusratsaleem123@gmail.com>
2026-09-18 20:53:58 +0500

    Update import statements for langchain modules

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_openai import ChatOpenAI
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # Configuration & Constants
